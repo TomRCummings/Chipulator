05b822932108fd1fcb6c99282403f01826518369
Thomas Cummings <cummings.t287@gmail.com>
2021-05-14 19:20:33 -0400

    Add opcodes, basic input, and console graphics
    Almost half the opcodes have been implemented. They need to be tested!
    The main loop has also been configured, along with console graphics
    output and basic inputs. These also need to be tested.

--- chip8.h
+++ chip8.h
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 class chip8 {
     //The current opcode to be executed
     unsigned short opcode;
@@ -9,7 +13,7 @@ class chip8 {
     unsigned short I;
     //Screen array (64x32 pixels)
     bool screen[2048];
-    //Key state array (chip-8 used hexadecimal keypad)
+    //Key state (chip-8 used hexadecimal keypad)
     bool key[16];
     //Program counter
     unsigned short pc;
@@ -45,9 +49,12 @@ class chip8 {
     void loadROM(unsigned char* program);
     unsigned char* getMemory();
     void cycle();
+    bool* getScreen();
+    void updateInput(unsigned char buttonPressed);
 };
 
 void chip8::initialize() {
+    srand (time(NULL));
     opcode = 0x0;
     I = 0x0;
     pc = 0x200;
@@ -79,4 +86,129 @@ void chip8::loadROM(unsigned char program[]) {
 
 unsigned char* chip8::getMemory() {
     return memory;
+}
+
+void chip8::cycle() {
+    bool keyPressed = false;
+    //Get the current opcode from memory using the program counter
+    opcode = memory[pc] << 8 | memory[pc+1];
+    //Decode and execute opcode in a switch statement
+    switch(opcode & 0xF000) {
+        case 0xA000: //0xAnnn: Set register I equal to nnn
+            I = opcode & 0x0FFF;
+            pc += 2;
+            break;
+        case 0xD000: //0xDxyn: Display n-byte sprite at (register x, register y), starting at memory location I
+            for (int i = 0; i < (opcode & 0x000F); i++) {
+                for (int j = 1; j < 8; j--) {
+                    if ((memory[I+i] >> (8-j)) & 0x0001 != 0) {
+                        if (screen[(((V[(opcode & 0x00F0)>>4]+i)*64)%2048)+((V[(opcode & 0x0F00)>>8]+(j-1))%64)]) {
+                            V[0xF] = 0xFF;
+                            screen[(((V[(opcode & 0x00F0)>>4]+i)*64)%2048)+((V[(opcode & 0x0F00)>>8]+(j-1))%64)] = false;
+                        } else {
+                            screen[(((V[(opcode & 0x00F0)>>4]+i)*64)%2048)+((V[(opcode & 0x0F00)>>8]+(j-1))%64)] = true;
+                        }
+                    }
+                }
+            }
+            pc += 2;
+            break;
+        case 0xF000:
+            switch(opcode & 0x00FF) {
+                case 0x000A: //0xFx0A: Wait for key press, store key value in register x
+                    for (int i = 0; i < 16; i++) {
+                        if (key[i]) {
+                            V[(opcode & 0x0F00) >> 8] = i;
+                            keyPressed = true;
+                        }
+                    }
+                    if (keyPressed) {
+                        pc += 2;
+                    }
+                    break;
+                case 0x0007: //0xFx07: Set register x to the delay timer value
+                    V[(opcode & 0x0F00) >> 8] = delayTimer;
+                    pc += 2;
+                    break; 
+                case 0x0029: //0xFx29: Value of I is set to the location of the sprite for the value of register x
+                    I = V[(opcode & 0x0F00) >> 8] * 5;
+                    pc += 2;
+                    break;
+                case 0x0033: //0xFx33: Store BCD representation of value at register x in memory location I, I+1, and I+2
+                    memory[I] = V[(opcode & 0x0F00) >> 8]/100; //100's digit at memory address I
+                    memory[I + 1] = (V[(opcode & 0x0F00) >> 8]/10)%10; //10's digit at memory address I+1
+                    memory[I + 2] = (V[(opcode & 0x0F00) >> 8]%100)%10; //1's digit at memory address I+2
+                    pc+=2;
+                    break;
+                case 0x0055: //0xFx55: Starting at memory address I, puts values from registers 0 to x into memory
+                    for (int i = 0; i <= ((opcode & 0x0F00) >> 8); i++) {
+                        memory[I+i] = V[i];
+                    }
+                    pc += 2;
+                    break;
+                case 0x0065: //0xFx65: Starting at memory address I, puts values from memory into registers 0 to x
+                    for (int i = 0; i <= ((opcode & 0x0F00) >> 8); i++) {
+                        V[i] = memory[I + i];
+                    }
+                    pc += 2;
+                    break;
+            }
+            break;
+        case 0x1000: //0x1nnn: Jump to address nnn
+            pc = opcode & 0x0FFF;
+            break;
+        case 0x6000: //0x6xkk: Set register x equal to kk
+            V[(opcode & 0x0F00) >> 8] = opcode & 0x00FF;
+            pc += 2;
+            break;
+        case 0x2000: //0x2nnn: Go to subroutine at address nnn
+            stack[stackPointer] = pc;
+            stackPointer++;
+            pc = opcode & 0x0FFF;
+            break;
+        case 0x7000: //0x7xkk: Add kk to the value in register x and put that into register x
+            V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x0F00) >> 8] + (opcode & 0x00FF);
+            pc += 2;
+            break;
+        case 0xC000: //0xCxkk: Generate random byte, AND it with kk, and store in register x
+            V[(opcode & 0x0F00) >> 8] = (rand() % 256) & (opcode & 0x00FF);
+            pc += 2;
+            break;
+
+        case 0x0000:
+            switch(opcode & 0x00FF) {
+                case 0x00E0: //0x00E0: Clear display
+                    for (int i = 0; i < 2048; i++) {
+                        screen[i] = false;
+                    }
+                    pc += 2;
+                    break;
+                
+                case 0x00EE: //0x00EE: Return from subroutine
+                    stackPointer--;
+                    pc = stack[stackPointer];
+                    break;
+
+                case 0x0000: //0x0nnn: Deprecated (used to jump to nnn)
+                    pc += 2;
+                    break;
+            }
+            break;
+
+        default: //Any unrecognizable opcode should throw an error and print the opcode
+            printf("Error with opcode: 0x%x\n", opcode);
+    }
+}
+
+bool* chip8::getScreen() {
+    return screen;
+}
+
+void chip8::updateInput(unsigned char buttonPressed) {
+    for (int i = 0; i < 16; i++) {
+        key[i] = false;
+    }
+    if (buttonPressed != 0xFF) {
+        key[buttonPressed] = true;
+    }
 }
--- chip8emu.cpp
+++ chip8emu.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <Windows.h>
 
 chip8 theChip8;
 
@@ -23,25 +24,33 @@ inline HexCharStruct hex(unsigned char _c)
 
 void saveChip8State(chip8 machine);
 void readFromRom(std::string filename, unsigned char* dataBuffer, size_t fileSize);
+void printScreenToConsole(bool* screenBuffer);
+unsigned char getInput();
 
 int main() {
-    theChip8.initialize();
+  theChip8.initialize();
 
-    std::string romFilePath = "random_number_test.ch8";
-    //Max number of bytes in a Chip-8 ROM
-    const size_t fileSize = 3586;
-    //Create program buffer
-    unsigned char dataBuffer[fileSize] = { 0 };
-    readFromRom(romFilePath, dataBuffer, fileSize);
-    //for (int i = 0; i < 3586; i++) {
-    //    std::cout << hex(dataBuffer[i]) << " ";
-    //    if (i % 100 == 0) {
-    //        std::cout << std::endl;
-    //    }
-    //}
-    theChip8.loadROM(dataBuffer);
-    saveChip8State(theChip8);
-    return 0;
+  std::string romFilePath = "random_number_test.ch8";
+  //Max number of bytes in a Chip-8 ROM
+  const size_t fileSize = 3586;
+  //Create program buffer
+  unsigned char dataBuffer[fileSize] = { 0 };
+  readFromRom(romFilePath, dataBuffer, fileSize);
+  //for (int i = 0; i < 3586; i++) {
+  //    std::cout << hex(dataBuffer[i]) << " ";
+  //    if (i % 100 == 0) {
+  //        std::cout << std::endl;
+  //    }
+  //}
+  theChip8.loadROM(dataBuffer);
+  for (;;) {
+    std::cout << "new cycle" << std::endl;
+    theChip8.cycle();
+    std::cout << std::string(100, '\n');
+    printScreenToConsole(theChip8.getScreen());
+    theChip8.updateInput(getInput());
+  }
+  return 0;
 }
 
 void saveChip8State(chip8 machine) {
@@ -60,4 +69,69 @@ void readFromRom(std::string filename, unsigned char* dataBuffer, size_t fileSiz
         //Read in bytes from file to buffer
         inf.read((char*)&dataBuffer[0], fileSize);
     }
+}
+
+void printScreenToConsole(bool* screenBuffer) {
+  for (int i = 0; i < 2048; i++) {
+    if (screenBuffer[i]) {
+      std::cout << "0";
+    } else {
+      std::cout << ".";
+    }
+    if (i%64 == 63) {
+      std::cout << std::endl;
+    }
+  }
+}
+
+unsigned char getInput() {
+  if(GetKeyState('0') & 0x8000) {
+    return 0x0;
+  }
+  if(GetKeyState('1') & 0x8000) {
+    return 0x1;
+  }
+  if(GetKeyState('2') & 0x8000) {
+    return 0x2;
+  }
+  if(GetKeyState('3') & 0x8000) {
+    return 0x3;
+  }
+  if(GetKeyState('4') & 0x8000) {
+    return 0x4;
+  }
+  if(GetKeyState('5') & 0x8000) {
+    return 0x5;
+  }
+  if(GetKeyState('6') & 0x8000) {
+    return 0x6;
+  }
+  if(GetKeyState('7') & 0x8000) {
+    return 0x7;
+  }
+  if(GetKeyState('8') & 0x8000) {
+    return 0x8;
+  }
+  if(GetKeyState('9') & 0x8000) {
+    return 0x9;
+  }
+  if(GetKeyState('A') & 0x8000) {
+    return 0xA;
+  }
+  if(GetKeyState('B') & 0x8000) {
+    return 0xB;
+  }
+  if(GetKeyState('C') & 0x8000) {
+    return 0xC;
+  }
+  if(GetKeyState('D') & 0x8000) {
+    return 0xD;
+  }
+  if(GetKeyState('E') & 0x8000) {
+    return 0xE;
+  }
+  if(GetKeyState('F') & 0x8000) {
+    return 0xF;
+  }
+  return 0xFF;
 }
